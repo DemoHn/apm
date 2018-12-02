ebb6f0cf5a2fd78497d474f1f0108da26b1ee0ae
DemoHn <demohn2016@gmail.com>
2018-12-03 01:16:21 +0800

    feat: update daemon log & stop recv SIGTERM signal

--- cli/daemon.go
+++ cli/daemon.go
@@ -1,15 +1,16 @@
 package cli
 
 import (
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/DemoHn/apm/mod/logger"
 	"github.com/DemoHn/apm/mod/master"
 	"github.com/urfave/cli"
 )
 
+var log *logger.Logger
 var daemonFlags = []cli.Flag{
 	cli.BoolFlag{
 		Name:  "debug,d",
@@ -24,6 +25,8 @@ func daemonHandler(c *cli.Context) error {
 	debugMode := c.Bool("debug")
 	// create & init master
 	m := master.New(debugMode)
+	// init logger
+	log = logger.Get()
 	err = m.Init(sockFile)
 	if err != nil {
 		return err
@@ -32,18 +35,18 @@ func daemonHandler(c *cli.Context) error {
 	go func() {
 		err = m.Listen()
 		if err != nil {
-			fmt.Println("[apm] server encounters an error:", err)
+			log.Error("[apm] server encounters an error:", err)
 			// send quit signal
 			quit <- os.Interrupt
 		}
 	}()
 
-	fmt.Println("[apm] listening to server")
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	log.Infof("[apm] listening to server: unix(%s)", sockFile)
+	signal.Notify(quit, syscall.SIGINT)
 	// wait for quit signal
-	<-quit
+	sig := <-quit
 
-	fmt.Println("[apm] going to shutdown")
+	log.Infof("[apm] going to shutdown due to recieve signal: %s", sig.String())
 	err = m.Shutdown()
 	if err != nil {
 		return err
